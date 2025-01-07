8d9cd2e9a39de18497089e0276639a12b3101b26
harshtrada <harsh17trada@gmail.com>
2025-01-07 15:43:00 +0530

    f

--- server/util.py
+++ server/util.py
@@ -27,13 +27,13 @@ def load_saved_artifacts():
     global  __data_columns
     global __locations
 
-    with open(r"server\artifacts\columns.json", "r") as f:
+    with open(r"\server\artifacts\columns.json", "r") as f:
         __data_columns = json.load(f)['data_columns']
         __locations = __data_columns[3:]  
 
     global __model
     if __model is None:
-        with open(r"server\artifacts\project.pickle", 'rb') as f:
+        with open(r"\server\artifacts\project.pickle", 'rb') as f:
             __model = pickle.load(f)
     print("loading saved artifacts...done")
 
